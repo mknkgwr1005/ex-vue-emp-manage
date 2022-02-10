24b553510f2e2e5e82f4491e0c392b62798b3e61
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-02-10 13:38:05 +0900

    ログアウト画面作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,10 @@ const routes: Array<RouteConfig> = [
   //   component: () => import("../components/LoginAdmin.vue"),
   // },
   {
+    path: "/logoutAdmin",
+    component: () => import("../views/LogoutAdmin.vue"),
+  },
+  {
     path: "/employeeDetail/:id",
     component: () => import("../views/EmployeeDetail.vue"),
   },
